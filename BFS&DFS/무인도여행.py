90a4f99f965f56ef37479a9a67f0a8a79224325f
karryred <ujin037@gmail.com>
2026-01-21 20:36:38 +0900

    프로그래머스 무인도여행

--- BFS&DFS/무인도여행.py
+++ BFS&DFS/무인도여행.py
@@ -1,33 +1,32 @@
-
 def solution(maps):
-    R=len(maps)
-    C=len(maps[0])
-    visited = [[0 for col in range(C)] for row in range(R)]
+    R = len(maps)
+    C = len(maps[0])
+    visited = [[0 for _ in range(C)] for _ in range(R)]
     answer = []
     
     for y in range(R):
         for x in range(C):
-            if maps[y][x]!='X' and not visited[y][x]:
+            if maps[y][x] != 'X' and not visited[y][x]:
+                max_value = 0
+                queue = [(y, x)]
+                visited[y][x] = 1
                 
-                max_value=0
-                queue=[(y,x)]
-                visited[y][x]=1 # 시작점 방문 처리
-                
-                while(queue):
-                    cur_y,cur_x=queue.pop(0)
-                    max_value+=int(maps[cur_y][cur_x])
+                while queue:
+                    cur_y, cur_x = queue.pop(0)
+                    max_value += int(maps[cur_y][cur_x])
                     
-                    for dx,dy in [(0,1), (0,-1), (1,0), (-1,0)]: # 상 하 좌 우
-                        ny=cur_x+dx
-                        nx=cur_y+dy
+                    # dy: 세로 변화량, dx: 가로 변화량
+                    for dy, dx in [(1, 0), (-1, 0), (0, -1), (0, 1)]: # 상 하 좌 우
+                        ny = cur_y + dy
+                        nx = cur_x + dx
                         
-                        if 0 <= ny < R and 0 <= nx < C and maps[ny][nx] != 'X' and not visited[ny][nx]:
-                            visited[ny][nx]=1 # 다음 점 방문 처리
-                            queue.append((ny,nx))
+                        # ny(세로)는 R과 비교, nx(가로)는 C와 비교
+                        if 0 <= ny < R and 0 <= nx < C:
+                            if maps[ny][nx] != 'X' and not visited[ny][nx]:
+                                visited[ny][nx] = 1
+                                queue.append((ny, nx))
                 
                 answer.append(max_value)
+    
     answer.sort()
-    if answer:
-        return answer
-    else:
-        return [-1]
+    return answer if answer else [-1]
